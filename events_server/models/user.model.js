401da84d4fedf09a901746fdd9952d8612a47080
Ekaterina Beliavskaja <84875077+beliavskaja@users.noreply.github.com>
2022-02-09 12:16:40 +0100

    Update user.model.js

--- events_server/models/user.model.js
+++ events_server/models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   email: {
     type: String,
-    // unique: true,
+    unique: true,
     required: true,
   },
   password: {
