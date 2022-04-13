23411bdbf5d90d172a7566bd171a6755478613f4
Ekaterina Beliavskaja <beliavskaja.ekaterina@gmail.com>
2022-04-13 09:18:18 +0200

    backend validation fix and adding page header

--- events_client/src/pages/events/Events.jsx
+++ events_client/src/pages/events/Events.jsx
@@ -1,18 +1,17 @@
-import { Button, Grid, Box } from "@mui/material";
+import { Grid, Box } from "@mui/material";
 import React from "react";
 import { useEffect, useState } from "react";
-import { Header } from "./Events.styled";
-import { Link } from "react-router-dom";
 import EventCard from "../../components/EventCard/EventCard";
 import getEvents from "../../services/api/axios/getEvents.api";
 import Pagination from "../../components/Pagination/Pagination";
 import NewEventModal from "../../components/NewEventModal/NewEventModal";
+import Header from "../../components/Header/Header";
 
 export default function Events({ logout, user }) {
   const [events, setEvents] = useState([]);
   const [total, setTotal] = useState(0);
 
-  const fetchEvents = (skip = 0, limit = 2) => {
+  const fetchEvents = (skip = 0, limit = 10) => {
     getEvents(skip, limit).then((data) => {
       setEvents(data.events);
       setTotal(data.total);
@@ -25,25 +24,18 @@ export default function Events({ logout, user }) {
 
   return (
     <>
-      <Header>
-        <nav
-          style={{
-            marginRight: "1rem",
-          }}
+      <Header logout={logout}/>
+      <Box marginLeft="10%" marginRight="10%" marginTop="74px" padding={1}>
+        <Box
+          paddingLeft={3}
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
         >
-          <Link to="/events">Events</Link> |{" "}
-          <Link to="/myevents">My Events</Link>
-        </nav>
-        <Button size="small" variant="contained" onClick={() => logout()}>
-          logout
-        </Button>
-      </Header>
-      <Box margin={5}>
-        <Box display="flex" justifyContent="space-between" alignItems="center">
           <h2>All Events</h2>
           {(user.roles || []).includes("ROLE_ADMIN") && <NewEventModal />}
         </Box>
-        <Grid container spacing={3} marginTop={1}>
+        <Grid container spacing={3}>
           {events.map((event) => (
             <Grid
               item
--- events_client/src/pages/myEvents/MyEvents.jsx
+++ events_client/src/pages/myEvents/MyEvents.jsx
@@ -1,24 +1,10 @@
-import { Button } from '@mui/material';
 import React from 'react';
-import { Header } from './MyEvents.styled';
-import { Link } from "react-router-dom";
+import Header from '../../components/Header/Header';
 
-export default function MyEvents ({ logout, user }) {
+export default function MyEvents ({ logout }) {
     return (
       <>
-      <Header>
-        <nav
-          style={{
-            marginRight: "1rem"
-          }}
-        >
-          <Link to="/events">Events</Link> |{" "}
-          <Link to="/myevents">My Events</Link>
-        </nav>
-        <Button size='small' variant="contained" onClick={() => logout()}>
-          logout
-        </Button>
-      </Header>
+      <Header logout={logout}/>
       <h2>My Events</h2>
       </>
     );
